4fe6f5c07c7d1776ae0319f3275cf2f2c032d7d0
kamay <xutaxkamay@gmail.com>
2020-03-16 23:32:11 +0100

    Using archlinux as base

--- rootfs_run/create_image.sh
+++ rootfs_run/create_image.sh
@@ -1,13 +1,13 @@
 ./umount.sh
 dd if=/dev/zero of=rootfs-image count=0 obs=1MB seek=2000
-sudo mkfs.ext4 rootfs-image
+sudo mkfs.ext4 rootfs-image -F
 ./mount.sh
-cd /uml_rootfs
-sudo wget -N -q -O /tmp/directory.html 'http://mir.archlinux.fr/iso/latest/'
+sudo wget -q -O /tmp/directory.html 'http://mir.archlinux.fr/iso/latest/'
 archlinuxbootstrap=$(sudo cat /tmp/directory.html| \
     grep bootstrap|grep -F 'tar.gz</a>'|awk -F'>' '{print $2}'|rev|cut -c 4-|rev)
-sudo wget -O /var/archlinux-rfs-x86_64.tar.gz http://mir.archlinux.fr/iso/latest/$archlinuxbootstrap
-sudo tar xf /var/archlinux-rfs-x86_64.tar.gz -C /uml_rootfs
+sudo wget -N -c -O /var/archlinux-rfs-x86_64.tar.gz http://mir.archlinux.fr/iso/latest/$archlinuxbootstrap
+sudo rm /uml_rootfs/* /uml_rootfs/.* -r
+sudo tar -xvf /var/archlinux-rfs-x86_64.tar.gz -C /uml_rootfs
 sudo mv /uml_rootfs/root.x86_64/* /uml_rootfs
 sudo rm /uml_rootfs/root.x86_64 -r
 ./populate.sh
--- rootfs_run/populate.sh
+++ rootfs_run/populate.sh
@@ -2,10 +2,11 @@ sudo /uml_rootfs/bin/arch-chroot \
 /uml_rootfs /bin/bash -c '
 pacman-key --init;
 pacman-key --populate archlinux;
-echo "Server = https://mirror.rackspace.com/archlinux/$repo/os/$arch" > /etc/pacman.d/mirrorlist;
+echo "Server = https://mirror.oldsql.cc/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist;
 pacman -Syyu --noconfirm;
-pacman -S base --noconfirm;
+pacman -S base base-devel vim iproute2 --noconfirm;
 exit'
 
-sudo $(cat init.sh) > /uml_rootfs/init.sh
+cat init.sh | sudo tee /uml_rootfs/init.sh
 sudo chmod 770 /uml_rootfs/init.sh
+echo '/dev/ubd0 / ext2 defaults 0 0' | sudo tee /uml_rootfs/etc/fstab
