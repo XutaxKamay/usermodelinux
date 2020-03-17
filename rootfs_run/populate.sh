8f9230b817c067721280f91c8c92cf269d28c675
kamay <xutaxkamay@gmail.com>
2020-03-18 00:17:53 +0100

    Added rng-tools so we can get directly ssh (entropy)

--- rootfs_run/populate.sh
+++ rootfs_run/populate.sh
@@ -6,7 +6,7 @@ export HOME=/root;
 pacman-key --init;
 pacman-key --populate archlinux;
 echo "Server = https://mirror.oldsql.cc/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist;
-pacman -Syyu base which iproute2 openssh zsh wget git vim --noconfirm;
+pacman -Syyu base which iproute2 openssh zsh wget git vim rng-tools --noconfirm;
 echo "Creating udb0";
 mknod --mode=777 /dev/ubd0 b 98 0;
 chown root:disk /dev/ubd0;
@@ -15,9 +15,10 @@ chsh -s /bin/zsh;
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended;
 sed -i -e "s/robbyrussell/rkj\-repos/g" /root/.zshrc;
 echo "/dev/ubd0 / ext4 defaults 0 0" > /etc/fstab;
-systemctl enable sshd;
 sed -i -e "s/#PermitRootLogin prohibit\-password/PermitRootLogin yes/g" /etc/ssh/sshd_config;        
 sed -i -e "/#ListenAddress 0/s/^#//g" /etc/ssh/sshd_config;        
+systemctl enable sshd;
+systemctl enable rngd;
 '
 ./network.sh
 ./umount.sh
--- rootfs_run/start.sh
+++ rootfs_run/start.sh
@@ -2,4 +2,4 @@ rm ~/.uml -rf
 ip=$(ip a | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 echo "Setting tuntap to host ip $ip"
 ./network.sh
-linux ubd0=rootfs-image mem=512M umid=linuxdbg_rootfs eth0=tuntap,,,$ip rw root=/dev/ubda debug gdb #con=pts con0=fd:0,fd:1
+linux ubd0=rootfs-image mem=512M umid=linuxdbg_rootfs eth0=tuntap,,,$ip rw root=/dev/ubda debug gdb con=pts con0=fd:0,fd:1
