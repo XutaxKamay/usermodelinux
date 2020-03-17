efe619a16bbc7931e3558c5067057bb18f29de98
kamay <xutaxkamay@gmail.com>
2020-03-17 19:38:18 +0100

    Changed to zsh shell

--- rootfs_run/create_image.sh
+++ rootfs_run/create_image.sh
@@ -2,13 +2,19 @@
 dd if=/dev/zero of=rootfs-image count=0 obs=1MB seek=2000
 sudo mkfs.ext4 rootfs-image -F
 ./mount.sh
+echo 'Finding version for root filesystem'
 sudo wget -q -O /tmp/directory.html 'http://mir.archlinux.fr/iso/latest/'
 archlinuxbootstrap=$(sudo cat /tmp/directory.html| \
     grep bootstrap|grep -F 'tar.gz</a>'|awk -F'>' '{print $2}'|rev|cut -c 4-|rev)
-sudo wget -N -c -O /var/archlinux-rfs-x86_64.tar.gz http://mir.archlinux.fr/iso/latest/$archlinuxbootstrap
-sudo rm /uml_rootfs/* /uml_rootfs/.* -r
-sudo tar -xvf /var/archlinux-rfs-x86_64.tar.gz -C /uml_rootfs
+echo 'Downloading/Updating archlinux bootstrap'
+sudo wget -q -N -c -O /var/archlinux-rfs-x86_64.tar.gz http://mir.archlinux.fr/iso/latest/$archlinuxbootstrap
+echo 'Extracting...'
+sudo tar -xf /var/archlinux-rfs-x86_64.tar.gz -C /uml_rootfs
+echo 'Moving folders...'
 sudo mv /uml_rootfs/root.x86_64/* /uml_rootfs
 sudo rm /uml_rootfs/root.x86_64 -r
+echo 'Populating'
 ./populate.sh
 ./umount.sh
+echo -e '\e[33mDone creating image!'
+notify-send 'Done creating image!'
--- rootfs_run/populate.sh
+++ rootfs_run/populate.sh
@@ -1,23 +1,29 @@
 ./mount.sh
 sudo /uml_rootfs/bin/arch-chroot \
 /uml_rootfs /bin/bash -c '
+export HOME=root
 pacman-key --init;
 pacman-key --populate archlinux;
 echo "Server = https://mirror.oldsql.cc/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist;
-pacman -Syyu base base-devel iproute2 vim --noconfirm;
+pacman -Syyu base which iproute2 openssh zsh wget git vim --noconfirm;
 echo "Creating udb0";
 mknod --mode=777 /dev/ubd0 b 98 0;
 chown root:disk /dev/ubd0;
 echo "root:tlxkjc" | chpasswd;
-exit'
-
-cat init.sh | sudo tee /uml_rootfs/init.sh
-sudo chmod 777 /uml_rootfs/init.sh
-sudo mkdir -p /uml_rootfs/etc/network
-echo '/dev/ubd0 / ext4 defaults 0 0' | sudo tee /uml_rootfs/etc/fstab
-echo 'nameserver 1.1.1.1' | sudo tee /uml_rootfs/etc/resolv.conf
-echo 'ip link set dev eth0 up' | sudo tee /uml_rootfs/etc/init/network.conf
-echo 'ip a add 192.168.1.24 dev eth0' | sudo tee -a /uml_rootfs/etc/init/network.conf
-echo 'ip route add default via 192.168.1.48' | sudo tee -a /uml_rootfs/etc/init/network.conf
-
+chsh -s /bin/zsh root;'
+# Relog so it logs in zsh instead
+sudo /uml_rootfs/bin/arch-chroot \
+/uml_rootfs /bin/zsh -c '
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)";
+echo "/dev/ubd0 / ext4 defaults 0 0" > /etc/fstab;
+echo "
+[Match]
+Name=eth0
+ 
+[Network]
+Address=192.168.1.24/24
+Gateway=192.168.1.48
+DNS=1.1.1.1
+" > /etc/systemd/network/eth0.network;
+sed -i -e "s/robbyrussell/rkj\-repos/g" /root/.zshrc'
 ./umount.sh
