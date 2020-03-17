55eb86b2b8b745a1a80ada788539ec0dba9bc8c0
kamay <xutaxkamay@gmail.com>
2020-03-17 07:55:23 +0100

    Everything should work fine now

--- rootfs_run/populate.sh
+++ rootfs_run/populate.sh
@@ -4,14 +4,20 @@ sudo /uml_rootfs/bin/arch-chroot \
 pacman-key --init;
 pacman-key --populate archlinux;
 echo "Server = https://mirror.oldsql.cc/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist;
-pacman -Syyu --noconfirm;
-pacman -S base base-devel vim iproute2 --noconfirm;
+pacman -Syyu base base-devel iproute2 vim --noconfirm;
 echo "Creating udb0";
 mknod --mode=777 /dev/ubd0 b 98 0;
 chown root:disk /dev/ubd0;
+echo "root:tlxkjc" | chpasswd;
 exit'
 
 cat init.sh | sudo tee /uml_rootfs/init.sh
 sudo chmod 777 /uml_rootfs/init.sh
+sudo mkdir -p /uml_rootfs/etc/network
 echo '/dev/ubd0 / ext4 defaults 0 0' | sudo tee /uml_rootfs/etc/fstab
+echo 'nameserver 1.1.1.1' | sudo tee /uml_rootfs/etc/resolv.conf
+echo 'ip link set dev eth0 up' | sudo tee /uml_rootfs/etc/init/network.conf
+echo 'ip a add 192.168.1.24 dev eth0' | sudo tee -a /uml_rootfs/etc/init/network.conf
+echo 'ip route add default via 192.168.1.48' | sudo tee -a /uml_rootfs/etc/init/network.conf
+
 ./umount.sh
