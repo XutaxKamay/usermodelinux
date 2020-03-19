8cb9c6902aa890498a1a7b42326dee532080dff5
kamay <xutaxkamay@gmail.com>
2020-03-19 01:56:20 +0100

    Added automation for building custom kernel module

--- rootfs_run/start.sh
+++ rootfs_run/start.sh
@@ -5,8 +5,10 @@ echo "Setting tuntap to host ip $ip"
 ./mount.sh
 OLDPWD=$PWD
 cd ~/projects/XLib/linux_kernel_module/um
-make
-sudo cp custom_module.ko /uml_rootfs/root
+if [ $? -eq 0 ]; then
+    make
+    sudo cp custom_module.ko /uml_rootfs/root
+fi
 cd $OLDPWD
 ./umount.sh
 linux ubd0=rootfs-image mem=512M umid=linuxdbg_rootfs eth0=tuntap,,,$ip rw root=/dev/ubda #con=pts con0=fd:0,fd:1
