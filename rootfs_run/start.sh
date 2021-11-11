eaa5475c770ed29761f4ae25a18f6be066e8e54e
XutaxKamay <admin@xutaxkamay.com>
2021-11-11 13:36:37 +0100

    Updated kernel config + preferred attach over pts

--- rootfs_run/start.sh
+++ rootfs_run/start.sh
@@ -2,4 +2,4 @@ sudo rm ~/.uml -rf
 ip=$(ip a | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -1)
 echo "Setting tuntap to host ip $ip"
 ./network.sh
-linux ubd0=rootfs-image mem=512M umid=linuxdbg_rootfs rw root=/dev/ubda eth0=tuntap,,,$ip #on=pts con0=fd:0,fd:1
+linux ubd0=rootfs-image mem=512M umid=linuxdbg_rootfs rw root=/dev/ubda eth0=tuntap,,,$ip con=pts con0=fd:0,fd:1
